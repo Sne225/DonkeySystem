0cf48ec6964b5cde3435814f9ae2c82c7156a9f1
Sinethemba <64343614+Sne225@users.noreply.github.com>
2023-10-09 14:26:26 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -7,8 +7,14 @@ import { getFirestore } from 'firebase/firestore';
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-
-;
+const firebaseConfig = {
+  apiKey: "AIzaSyCPn8Y47KaPBy8dppFf7SZ5Ni5cUMJ1XwY",
+  authDomain: "donkeysystem-6b9fc.firebaseapp.com",
+  projectId: "donkeysystem-6b9fc",
+  storageBucket: "donkeysystem-6b9fc.appspot.com",
+  messagingSenderId: "311146237020",
+  appId: "1:311146237020:web:7de2c6d0f9bb5d020eea99"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
